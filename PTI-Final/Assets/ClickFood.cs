c8d970928465a6252478d199b67cd17c3e7854f7
rachelli <rl2563@nyu.edu>
2019-11-22 15:30:58 -0500

    project

---
+++ PTI-Final/Assets/ClickFood.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickFood : MonoBehaviour
+{
+    private Camera myCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetMouseButtonDown(0))
+       {
+           Ray myRay = myCam.ScreenPointToRay(Input.mousePosition);
+           RaycastHit hitInfo = new RaycastHit();
+           
+          if(Physics.Raycast(myRay, out hitInfo))
+              if (hitInfo.collider != null)
+              {
+                  if (hitInfo.collider.CompareTag("food"))
+                  {
+                      GameObject foodClicked = hitInfo.collider.gameObject;
+                      foodClicked.SetActive(false);
+                  }
+              }
+       }
+    }
+}
