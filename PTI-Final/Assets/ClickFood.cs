59b900b2b946f445e7099af0810c0c3a81057e63
shuyi <mycalendula@gmail.com>
2019-12-05 21:46:05 -0500

    modified to using keys.

--- PTI-Final/Assets/ClickFood.cs
+++ PTI-Final/Assets/ClickFood.cs
@@ -5,15 +5,37 @@ using UnityEngine;
 public class ClickFood : MonoBehaviour
 {
     private Camera myCam;
+
+    public List<GameObject> foodList;
+    public List<string> keyList;
+
+    public GameObject Course2;
+    public GameObject Course1;
+
+    private List<bool> toogle = new List<bool>();
+    
     // Start is called before the first frame update
     void Start()
     {
         myCam = Camera.main;
+
+        for (int i = 0; i < foodList.Count; i++)
+        {
+            toogle.Add(false);
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown("0"))
+        {
+            Course2.SetActive(true);
+            Course1.SetActive(false);
+        }
+        
+        //using mouse
        if(Input.GetMouseButtonDown(0))
        {
            Ray myRay = myCam.ScreenPointToRay(Input.mousePosition);
@@ -29,5 +51,19 @@ public class ClickFood : MonoBehaviour
                   }
               }
        }
+
+       //using keyboard
+       for (int i = 0; i < foodList.Count; i++)
+       {
+           if (Input.GetKeyDown(keyList[i]))
+           {
+               
+               foodList[i].SetActive(toogle[i]);
+               toogle[i] = !toogle[i];
+           }
+        
+       }
+       
+       
     }
 }
